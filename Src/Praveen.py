021e47350a292f3b144c76d7cc48a355d0e516be
DMUPraveen <dmu.praveen@gmail.com>
2021-02-22 22:09:21 +0530

    Unified the Stacks

--- Src/Praveen.py
+++ Src/Praveen.py
@@ -1,5 +1,5 @@
 from Samith import file_handling
-
+import os
 
 class Pizza_Key_Maker:
     def __init__(self):
@@ -21,38 +21,33 @@ class Pizza_Key_Maker:
                 
 class DeliverSystem:
     def __init__(self,twoSize,threeSize,fourSize):
-        self.two_stack = [] #contains a pair (set(ingrediants),pizza ids)
-        self.three_stack = []
-        self.four_stack = []
-        self.two_max_size = twoSize
-        self.three_max_size = threeSize
-        self.four_max_size = fourSize
-        self.current_Delviery = None
+        self.Stack = [] # [set()->Ingrediants , []->PizzaIds]
+        self.max_Sizes = [0,0,twoSize,threeSize,fourSize]
+        self.current_Delviery = []
         self.current_Delivery_team_type = -1
+        self.Sizes = [0,0,0,0,0]
 
     def push_delivery(self,team_type):
-        if(team_type == 2):
-            self.two_stack.append([set(),[]])
-            self.current_Delviery = self.two_stack[-1]
-            if(len(self.two_stack) > self.two_max_size):
-                Exception("two stack overflow")
-        elif(team_type == 3):
-            self.three_stack.append([set(),[]])
-            self.current_Delviery = self.three_stack[-1]
-            if(len(self.three_stack) > self.three_max_size):
-                Exception("two stack overflow")
-        elif(team_type == 4):
-            self.four_stack.append([set(),[]])
-            self.current_Delviery = self.four_stack[-1]
-            if(len(self.four_stack) > self.four_max_size):
-                Exception("four stack overflow")
-        else:
-            Exception("Unknown Team")
+        if(self.current_Delivery_team_type != -1):
+            if(len(self.current_Delviery[1]) != self.current_Delivery_team_type):
+                raise Exception("Previous Team unfilled")
+        if not(team_type ==2 or team_type != 3 or team_type!=4):
+            raise Exception("Unknown Team")
+        
+        self.current_Delviery = []
+        self.current_Delivery_team_type = team_type
+        if(self.Sizes[team_type] > self.max_Sizes[team_type]):
+            raise Exception(f"team_type {team_type} overflow")
+        self.Stack.append([set(),[]])
+        self.current_Delviery = self.Stack[-1]
+        
+        
+
         
 
 
     def add_Pizza_to_delivery(self,PizzaId,toppingSet):
-        if(len(self.current_Delviery[1]) > self.current_Delivery_team_type):
+        if(len(self.current_Delviery[1]) >= self.current_Delivery_team_type):
             return False
         self.current_Delviery[0] = set.union(self.current_Delviery[0],toppingSet)
         self.current_Delviery[1].append(PizzaId)
@@ -60,14 +55,14 @@ class DeliverSystem:
 
     
 
-
+os.chdir("C:\\Users\\USER\\Desktop\\Competition\\HashCode\\Pizza\\Src")
 no_pizzas,teams_2,teams_3,teams_4,list_of_pizzas = file_handling()
 PizzaData = Pizza_Key_Maker()
 for id,line in enumerate(list_of_pizzas):
     PizzaData.add_pizza(id,line)
 
 deliveries = DeliverSystem(teams_2,teams_3,teams_4)
-
+#deliveries.push_delivery(1)
 
 
 
